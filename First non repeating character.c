c1ca66cd9ac251888df5be50d105bab0b8cf5669
Saravanan S <152407146+Saravanan7781@users.noreply.github.com>
2024-07-14 21:20:08 +0530

    Create First non repeating character.c

---
+++ First non repeating character.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<string.h>
+
+int firstUniqChar(char* s) {
+    int len = strlen(s);
+    int result =0;
+    if(strlen(s)==1){
+        return 0;
+    }
+    for(int i=0;i<len;i++){
+        result = 0;
+        for(int j=0;j<len;j++){
+            if(i==j)
+            continue;
+            if(s[i]==s[j]){
+            result = 1;
+            break;
+            }
+        }
+        if(result==0)
+        return i;
+    }
+    return -1;
+    
+}
+
+int main()
+{
+    char chara[] = "leetcode";
+    printf("index at:%d",firstUniqChar(chara));
+
+    return 0;
+}
